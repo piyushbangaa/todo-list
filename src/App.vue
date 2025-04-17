bd70540edf0b1e681e4b6c2be971421138ba04e7
piyushbangaa <piyushbanga2003@gmail.com>
2025-04-18 03:00:13 +0530

    added input box component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="max-w-md p-4 bg-white rounded-xl shadow">
+      <h1 class="text-xl font-bold text-center mb-4">Vue To-Do List</h1>
+
+      <TodoInput @add-task="addTask" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import TodoInput from './components/TodoInput.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoInput,
+  },
+  methods: {
+    addTask(task) {
+      console.log('Task added:', task);
+    },
+  },
+};
+</script>
+
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="mb-4">
+      <input
+        v-model="task"
+        @keyup.enter="addTask"
+        type="text"
+        class="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+        placeholder="Add a new task"
+      />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        task: '',
+      };
+    },
+    methods: {
+      addTask() {
+        if (this.task.trim()) {
+          this.$emit('add-task', this.task); 
+          this.task = ''; 
+        }
+      },
+    },
+  };
+  </script>
+  
